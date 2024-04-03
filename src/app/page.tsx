db6a8487489aadaba82c15ae410e002609b299b4
Sakib Hasan <90200484+shakibhasan09@users.noreply.github.com>
2024-04-04 05:22:47 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -278,9 +278,9 @@ export default async function Home() {
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Shakib, a software designer and entrepreneur based in Dhaka,
-            Bangladesh. I’m the founder and CEO of Skymap IT LLC, where we
-            develop technologies that empower regular people to explore space on
-            their own terms.
+            Bangladesh. I’m the founder and CEO of Skymap IT, where we develop
+            technologies that empower regular people to explore space on their
+            own terms.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
